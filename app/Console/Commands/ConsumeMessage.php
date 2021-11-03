5cbae18081787e48cc0e0f4b1093f8dbc8d1ed94
FrederST <freder.hernandez@correounivalle.edu.co>
2021-11-03 11:20:58 -0500

    fix: remove comments

--- app/Console/Commands/ConsumeMessage.php
+++ app/Console/Commands/ConsumeMessage.php
@@ -9,43 +9,23 @@ use E4\Messaging\MessageBroker;
 use Illuminate\Console\Command;
 use PhpAmqpLib\Message\AMQPMessage;
 use E4\Messaging\Utils\MsgSecurity;
-use Illuminate\Support\Facades\Event;
 
 class ConsumeMessage extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+
     protected $signature = 'consume:message';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Consume Rabbit Queue Messages';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle(MessageBroker $messageBroker)
     {
         $messageBroker->consume(function (AMQPMessage $message) {
-            // $this->error($message->body);
             $messageSecurity = new MsgSecurity(
                 config('messagingapp.encryption.secretKey'),
                 config('messagingapp.encryption.method'),
@@ -56,12 +36,8 @@ class ConsumeMessage extends Command
             );
             $events =  config('messagingapp.events');
             if (array_key_exists($message->getRoutingKey(), $events)) {
-                $this->error($events[$message->getRoutingKey()]);
-                // Event::dispatch($events[$message->getRoutingKey()], $messageSecurity->prepareMsgToReceive($message->body)->jsonSerialize());
                 event(new $events[$message->getRoutingKey()]($messageSecurity->prepareMsgToReceive($message->body)->jsonSerialize()));
             }
-            // $this->error($message->getRoutingKey());
-            // $message->ack();
         });
     }
 }
